35326f19a1f3692e5f27ecb330afc0447f045292
Weronika Jaworek <s18630@pjwstk.edu.pl>
2021-01-19 06:37:31 +0100

    Początkowe zapytania

---
+++ Cwiczenie9/LinqConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            d.Przyklad1();
+            Console.WriteLine();
+            d.Przyklad2();
+            Console.WriteLine();
+            d.Przyklad3();
+            Console.WriteLine();
+            d.Przyklad4();
+            Console.WriteLine();
+            d.Przyklad5();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(); 
+            Console.WriteLine();
+            d.Przyklad6();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad7();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(d.Przyklad8());
+            Console.WriteLine();
+            d.Przyklad9();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            d.Przyklad11();
+
+
+
+
+
+
+
+
+        }
+    }
+}
